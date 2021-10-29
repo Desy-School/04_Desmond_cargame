c2deac29290fb4fd70d17b7f6a1c90b6018282a1
Desy-School <Desy.student@gmail.com>
2021-10-29 15:28:23 +0800

    content filled

--- dealer.js
+++ dealer.js
@@ -7,6 +7,82 @@ var cars_sold = 0;
 var amount = 0;
 var brandcost = new Array(72500, 23930, 31260, 43990);
 
+var questions = [
+	{
+		"qns":"___ you happy today?",
+		"no":"1",
+		"correctAns":"c",
+		"points":"2",
+		"choices" : [{
+						"title":"at",
+						"no":"a"
+				},
+				{
+						"title":"was",
+						"no":"b"
+				},
+				{
+						"title":"are",
+						"no":"c"
+				},
+				{
+						"title":"were",
+						"no":"d"
+				}
+			]
+	},
+	{
+		"qns":"Will you come ___ class today?",
+		"no":"2",
+		"correctAns":"d",
+		"points":"2",
+		"choices" : [{
+						"title":"was",
+						"no":"a"
+				},
+				{
+						"title":"in",
+						"no":"b"
+				},
+				{
+						"title":"at",
+						"no":"c"
+				},
+				{
+						"title":"to",
+						"no":"d"
+				}
+			]
+	},
+	{
+		"qns":"Where ___ you from?",
+		"no":"3",
+		"correctAns":"c",
+		"points":"2",
+		"choices" : [{
+						"title":"to",
+						"no":"a"
+				},
+				{
+						"title":"at",
+						"no":"b"
+				},
+				{
+						"title":"are",
+						"no":"c"
+				},
+				{
+						"title":"were",
+						"no":"d"
+				}
+			]
+		}
+];
+
+var qnsIndex = 0;
+var selections =[];
+var currentClient = null;
+
 function newClient(){
 	var preference = Math.floor((Math.random()*4));
 	var time = Math.floor((Math.random()*10000)+1);
@@ -54,8 +130,12 @@ function makeCarBoxesDroppable(brand) {
 						$dragBox.css(removeMarginStyle);
 						count--;
 						$dragBox.addClass('selected');
+						currentClient = $dragBox;
+						next_qns();
+						var dialogOption = { scrolling: 'no' };
+						$.fancybox.open('#mcq',dialogOption);
 					}
-	};
+				};
 	$carBoxes.droppable(options);
 }
 function makeExitDroppable() {
@@ -128,11 +208,11 @@ function showCashierDialog(dragClient) {
 				cars_sold += 1;
 				amount += calcost(dragClient);
 				update();
-				removeBox(dragClient, -235);
+				removeBox(dragClient, "-=210");
 				$( this ).dialog( "close" );
 			},
 			"No and Exit": function() {
-				removeBox(dragClient, -250);
+				removeBox(dragClient, "-=210");
 				$( this ).dialog( "close" );
 			}
 		},
@@ -200,4 +280,163 @@ function hideAllPages() {
 			currentPage.hide();
 		}
 	);
+}
+
+function next_qns() {
+	if(qnsIndex < questions.length) {
+		var current = questions[qnsIndex];
+		var questionTitle = $("#questionTitle");
+		questionTitle.html((qnsIndex+1) + ". " + current.qns);
+
+		var optA = current.choices[0];
+		var optA_Box = $("#optionA");
+		optA_Box.html(optA.title);
+
+		var optB = current.choices[1];
+		var optB_Box = $("#optionB");
+		optB_Box.html(optB.title);
+
+		var optC = current.choices[2];
+		var optC_Box = $("#optionC");
+		optC_Box.html(optC.title);
+
+		var optD = current.choices[3];
+		var optD_Box = $("#optionD");
+		optD_Box.html(optD.title);
+
+		optA_Box.css("background-color", "darkseagreen");
+		optB_Box.css("background-color", "darkseagreen");
+		optC_Box.css("background-color", "darkseagreen");
+		optD_Box.css("background-color", "darkseagreen");
+		qnsIndex++;
+	}
+	
+	else {
+		qnsIndex = 0;
+		var totalScore = 0;
+		for(var i=0;i<selections.length;i++) {
+			var selection = selections[i];
+			var question = null;
+			for(var h=0;h<questions.length;h++) {
+				var q = questions[h];
+				if(q.no == selection.qnsNo) {
+					question = q;
+					break;
+				}
+			}
+			if(selection.selected == question.correctAns) {
+				totalScore += parseInt(question.points);
+			}
+		}
+		
+		var totalMarks = 0;
+		for(var h=0;h<questions.length;h++) {
+			var q = questions[h];
+			totalMarks += parseInt(question.points);
+		}
+		var percScore = (totalScore / totalMarks) * 100;
+		var failed = true;
+		
+		if(percScore > 50) {
+			failed = false;
+		}
+		
+		var resultPanel = $("#result-panel");
+		resultPanel.css("display","block");
+		
+		var questionPanel = $("#question-panel");
+		questionPanel.css("display","none");
+		
+		var scoreBox = $("#totalScore");
+		scoreBox.html("Score: " + totalScore + "/" + totalMarks);
+		
+		var result = "";
+		if(failed == true) {
+			result = "<img src='images/tenor.gif' width='250' height='200' alt='failed'/>";
+		}
+		else {
+			result = "<img src='images/excellent.png' width='250' height='200' alt='passed'/>";
+		}
+		var myresult = $("#myresult");
+		myresult.html(result);
+		
+		var closeResultButton = $("#closeResultButton");
+		closeResultButton.click(function() {
+			$.fancybox.close();
+			selections = [];
+			questionPanel.css("display","block");
+			resultPanel.css("display","none");
+			
+			var clientX = currentClient.offset().left;
+			var clientY = currentClient.offset().top;
+			if(failed == true){
+				var exit = $("#exit_img_holder");
+				var exitX = exit.offset().left;
+				var exitY = exit.offset().top;
+				
+				var diffX = exitX - clientX;
+				var diffY = exitY - clientY;
+				
+				currentClient.css("zIndex", 3000);
+				currentClient.animate(
+								{
+									left: "+=" + diffX,
+									top: "+=" + diffY,
+								},
+								1000).fadeOut(2000, function() {
+											count--;
+											newClient();
+										}
+									);
+			}
+			else {
+				var cashier = $("#cashier_img_holder");
+				var cashierX = cashier.offset().left;
+				var cashierY = cashier.offset().top;
+				
+				var diffX = cashierX - clientX;
+				var diffY = cashierY - clientY;
+				
+				currentClient.css("zIndex", 3000);
+				currentClient.animate(
+								{
+									left: "+=" + diffX,
+									top: "+=" + diffY,
+								},
+								1000, function() {
+									showCashierDialog(currentClient);
+					});
+			}
+			closeResultButton.unbind();
+		});
+	}
+}
+
+function ansBox_click(selectedChoice) {
+	var optA_Box = $("#optionA");
+	var optB_Box = $("#optionB");
+	var optC_Box = $("#optionC");
+	var optD_Box = $("#optionD");
+	optA_Box.css("background-color","darkseagreen");
+	optB_Box.css("background-color","darkseagreen");
+	optC_Box.css("background-color","darkseagreen");
+	optD_Box.css("background-color","darkseagreen");
+	if(selectedChoice == "a") {
+		optA_Box.css("background-color","khaki");
+	}
+	else if(selectedChoice == "b") {
+		optB_Box.css("background-color","khaki");
+	}
+	else if(selectedChoice == "c") {
+		optC_Box.css("background-color","khaki");
+	}
+	else if(selectedChoice == "d") {
+		optD_Box.css("background-color","khaki");
+	}
+	var selection = {
+						"qnsNo" : qnsIndex,
+						"selected" : selectedChoice,
+						"qnsType" : "mcq"
+					};
+	selections.push(selection);
 }
